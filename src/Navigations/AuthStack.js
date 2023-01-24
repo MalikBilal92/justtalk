1ea7565fa149c511ff9e6cf53e4cbfc4cca52dd2
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-25 00:03:56 +0500

    Create AuthStack.js

---
+++ src/Navigations/AuthStack.js
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { InitialScreen, Login, Signup } from "../Screens";
+import navigationStrings from "../Constants/navigationStrings";
+export default function (Stack) {
+  return (
+    <>
+      <Stack.Screen
+        name={navigationStrings.INITIAL_SCREEN}
+        component={InitialScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={navigationStrings.LOGIN}
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={navigationStrings.SIGNUP}
+        component={Signup}
+        options={{ headerShown: false }}
+      />
+    </>
+  );
+}
