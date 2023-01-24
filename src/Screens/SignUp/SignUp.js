2969de38519a959131fbcf16fce9deedd81a585f
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-25 00:04:02 +0500

    Create SignUp.js

---
+++ src/Screens/SignUp/SignUp.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import styles from "../../Home/styles";
+const Signup = () => {
+  return (
+    <View style={styles.myStyle}>
+      <Text>signup</Text>
+    </View>
+  );
+};
+
+export default Signup;
+
+// const styles = StyleSheet.create({});
