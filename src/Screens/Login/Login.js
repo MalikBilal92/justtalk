1724438bdb1cb9a6574301ee598c981b3282300f
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 23:17:54 +0500

    Update Login.js

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -8,65 +8,43 @@ const Login = () => {
 	return (
 		<View style={styles.myStyle}>
 			<Text>Code working Perfectly but your partner is Just Dash</Text>
-			{/* <Image source={ImagesPath.iconImage} style={{ marginTop: 100 }} />
-			<Text style={style.txt}>Welcome to Login!</Text>
+			<Image source={ImagesPath.iconImage} style={{ marginTop: 100 }} />
+			<Text style={cisco.txt}>Welcome to Login!</Text>
 			<View>
 				<Text>Email</Text>
 
-				<TextInput
-					style={{
-						paddingHorizontal: 250,
-						borderWidth: 1,
-						borderRadius: 1,
-						fontSize: 18,
-						paddingVertical: 7,
-						height: 43,
-						fontSize: 14,
-						borderRadius: 5,
-						borderWidth: 1,
-						borderColor: "#eaeaea",
-						backgroundColor: "#fafafa",
-						paddingLeft: 10,
-						marginTop: 5,
-						marginBottom: 5,
-					}}
-					placeholder="Username"
-				/>
+				<TextInput style={cisco.TextInput} placeholder="Username" />
 			</View>
 			<View>
 				<Text>Password</Text>
 				<TextInput
-					style={{
-						paddingHorizontal: 250,
-						borderWidth: 1,
-						borderRadius: 1,
-						fontSize: 18,
-						paddingVertical: 7,
-						height: 43,
-						fontSize: 14,
-						borderRadius: 5,
-						borderWidth: 1,
-						borderColor: "#eaeaea",
-						backgroundColor: "#fafafa",
-						paddingLeft: 10,
-						marginTop: 5,
-						marginBottom: 5,
-					}}
+					style={cisco.TextInput}
+					secureTextEntry={true}
 					placeholder="Password"
 				/>
 			</View>
-			<Text style={{ fontVariant: "bold" }}>Forgot Password?</Text> */}
 		</View>
 	);
 };
-const style = StyleSheet.create({
+const cisco = StyleSheet.create({
 	txt: {
 		fontSize: "30",
 		fontWeight: "600",
 		marginTop: 50,
 		marginLeft: 20,
 	},
-	TextInput: {},
+	TextInput: {
+		width: 340,
+		paddingVertical: 7,
+		fontSize: 18,
+		height: 43,
+		fontSize: 14,
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: "#eaeaea",
+		backgroundColor: "#fafafa",
+		paddingLeft: 10,
+	},
 });
 
 export default Login;
