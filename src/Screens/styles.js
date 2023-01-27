de0018fbf6173056c79a7baafb341d847e6f0a81
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 23:21:47 +0500

    Create styles.js

---
+++ src/Screens/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	myStyle: {
+		flex: 1,
+		backgroundColor: "white",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btnStyle: {
+		backgroundColor: "white",
+		padding: 8,
+		borderRadius: 6,
+	},
+	txt: {
+		fontSize: "30",
+		fontWeight: "600",
+		marginTop: 50,
+		marginLeft: 20,
+	},
+	TextInput: {
+		width: 340,
+		paddingVertical: 7,
+		fontSize: 18,
+		height: 43,
+		fontSize: 14,
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: "#eaeaea",
+		backgroundColor: "#fafafa",
+		paddingLeft: 10,
+	},
+});
+
+export default styles;
