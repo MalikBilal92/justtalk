f40ab4ae18b0c6406e10724befb7d685c07bf8b6
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 21:18:22 +0500

    Update InitialScreen.js

--- src/Screens/InitialScreen/InitialScreen.js
+++ src/Screens/InitialScreen/InitialScreen.js
@@ -1,49 +1,53 @@
 import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  SafeAreaView,
-  TouchableOpacity,
+	StyleSheet,
+	Text,
+	View,
+	ImageBackground,
+	SafeAreaView,
+	TouchableOpacity,
 } from "react-native";
 import React from "react";
 import ImagesPath from "../../Constants/ImagesPath";
 import colors from "../../styles/colors";
-const InitialScreen = () => {
-  ImagesPath;
-  return (
-    <ImageBackground
-      style={{ flex: 1, justifyContent: "flex-end" }}
-      source={ImagesPath.bgImage}
-    >
-      <SafeAreaView>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.text}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn1}>
-          <Text style={styles.text}>Sign Up</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </ImageBackground>
-  );
+const InitialScreen = ({ navigation }) => {
+	ImagesPath;
+	// const { navigation } = props.navigation;
+	return (
+		<ImageBackground
+			style={{ flex: 1, justifyContent: "flex-end" }}
+			source={ImagesPath.bgImage}
+		>
+			<SafeAreaView>
+				<TouchableOpacity
+					style={styles.btn}
+					onPress={() => navigation.navigate("Home")}
+				>
+					<Text style={styles.text}>Login</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.btn1}>
+					<Text style={styles.text}>Sign Up</Text>
+				</TouchableOpacity>
+			</SafeAreaView>
+		</ImageBackground>
+	);
 };
 
 export default InitialScreen;
 
 const styles = StyleSheet.create({
-  btn: {
-    height: 48,
-    backgroundColor: colors.red,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  btn1: {
-    height: 48,
-    backgroundColor: colors.blue,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: colors.white,
-  },
+	btn: {
+		height: 48,
+		backgroundColor: colors.red,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	btn1: {
+		height: 48,
+		backgroundColor: colors.blue,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		color: colors.white,
+	},
 });
