89742dc21600fd9d4cb3309ebb59b38a794c20f8
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 21:56:59 +0500

    Update AuthStack.js

--- src/Navigations/AuthStack.js
+++ src/Navigations/AuthStack.js
@@ -2,7 +2,6 @@ import React from "react";
 import { InitialScreen, Login, Signup, Home } from "../Screens";
 import navigationStrings from "../Constants/navigationStrings";
 export default function (Stack) {
-<<<<<<< Updated upstream
 	return (
 		<>
 			<Stack.Screen
@@ -27,25 +26,5 @@ export default function (Stack) {
 			/>
 		</>
 	);
-=======
-  return (
-    <>
-      <Stack.Screen
-        name={navigationStrings.LOGIN}
-        component={Login}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name={navigationStrings.INITIAL_SCREEN}
-        component={InitialScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name={navigationStrings.SIGNUP}
-        component={Signup}
-        options={{ headerShown: false }}
-      />
-    </>
-  );
->>>>>>> Stashed changes
+
 }
