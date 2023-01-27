73b1a590a5868ae30c71d24f41155c042487413d
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 22:18:16 +0500

    Update styles.js

--- src/Screens/Home/styles.js
+++ src/Screens/Home/styles.js
@@ -1,17 +1,17 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  myStyle: {
-    flex: 1,
-    backgroundColor: "white",
-    // justifyContent: "center",
-    alignItems: "center",
-  },
-  btnStyle: {
-    backgroundColor: "white",
-    padding: 8,
-    borderRadius: 6,
-  },
+	myStyle: {
+		flex: 1,
+		backgroundColor: "white",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btnStyle: {
+		backgroundColor: "white",
+		padding: 8,
+		borderRadius: 6,
+	},
 });
 
 export default styles;
