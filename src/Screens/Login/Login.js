b9bc6f7e8665b59ba8204a059db161899b4c4011
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 21:33:21 +0500

    Update Login.js

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImagePickerIOS, StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
+<<<<<<< Updated upstream
 import styles from "../Home/styles";
 
 const Login = () => {
@@ -9,5 +10,60 @@ const Login = () => {
 		</View>
 	);
 };
+=======
+import styles from "../../Home/styles";
+import ImagesPath from "../../Constants/ImagesPath";
+import { Image } from "react-native";
+const Login = () => {
+  return (
+    <View style={styles.myStyle}>
+      <Image source={ImagesPath.iconImage}
+        style={{ marginTop: 100 }}
+      />
+      <Text style={style.txt}>Welcome to Login!</Text>
+<View>
+<Text>Email</Text>
+
+      <TextInput style={{paddingHorizontal:250,borderWidth:1,borderRadius:1,fontSize:18,paddingVertical:7, height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#eaeaea",
+    backgroundColor: "#fafafa",
+    paddingLeft: 10,
+    marginTop: 5,
+    marginBottom: 5,}} placeholder="Username" />
+    </View>
+    <View>
+      <Text>Password</Text>
+     <TextInput style={{paddingHorizontal:250,borderWidth:1,borderRadius:1,fontSize:18,paddingVertical:7, height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#eaeaea",
+    backgroundColor: "#fafafa",
+    paddingLeft: 10,
+    marginTop: 5,
+    marginBottom: 5,}} placeholder="Password" />
+     </View>
+     <Text style={{fontVariant:"bold"}}>Forgot Password?</Text>
+    </View>
+
+  );
+
+}
+const style = StyleSheet.create({
+  txt: {
+    fontSize: "30",
+    fontWeight: "600",
+    marginTop: 50,
+    marginLeft: 20
+  },
+  TextInput:{
+    
+  }
+
+})
+>>>>>>> Stashed changes
 
 export default Login;
