28a7b4c8be76b81afd5c61887a46227ebdedb92d
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 23:21:35 +0500

    Update Home.js

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
-import styles from "./styles";
+import styles from "../styles";
 const Home = () => {
 	const [myNum, setNum] = useState(10);
 	const changeNum = () => {
