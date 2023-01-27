1ee9ab2056cf581aeeec627a5d600c9073a21657
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 21:18:19 +0500

    Update index.js

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -2,3 +2,4 @@
 export { default as InitialScreen } from "./InitialScreen/InitialScreen";
 export { default as Login } from "./Login/Login";
 export { default as Signup } from "./SignUp/SignUp";
+export { default as Home } from "./Home/Home";
