d33cff096eaa6b21e0de29be8db4e3b8ee665439
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-27 22:18:12 +0500

    Update Home.js

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 import styles from "./styles";
 const Home = () => {
-  const [myNum, setNum] = useState(10);
-  const changeNum = () => {
-    setNum(20);
-  };
-  console.log("numValue", myNum);
+	const [myNum, setNum] = useState(10);
+	const changeNum = () => {
+		setNum(20);
+	};
+	console.log("numValue", myNum);
 
-  return (
-    <View style={styles.myStyle}>
-      <Text>Please subscribe my channel</Text>
-      <Button title="Press me" onPress={changeNum} />
-    </View>
-  );
+	return (
+		<View style={styles.myStyle}>
+			<Text>Please subscribe my channel</Text>
+			<Button title="Press me" onPress={changeNum} />
+		</View>
+	);
 };
 
 //make this component available to the app
