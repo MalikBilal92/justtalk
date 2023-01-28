d5ba58fe83d2bc92c00671c91abf2f1e1340f3b4
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-28 20:55:44 +0500

    Update ImagesPath.js

--- src/Constants/ImagesPath.js
+++ src/Constants/ImagesPath.js
@@ -1,5 +1,5 @@
 export default {
 	bgImage: require("../assets/Images/backgroundImage.jpg"),
 	iconImage: require("../assets/Images/iconText.png"),
-	confirmationDone: require("../assets/Images/done.png"),
+	checkCircle: require("../assets/Images/check-circle/ss.png"),
 };
