97a1ea9db9d0facf3dcf4fec4ce2599965c873f3
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-28 20:56:56 +0500

    Create newaccount.js

---
+++ src/Screens/New Account/newaccount.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const newaccount = ({navigation}) => {
+  return (
+    <View>
+      <Text>newaccount</Text>
+    </View>
+  )
+}
+
+export default newaccount
+
+const styles = StyleSheet.create({})
