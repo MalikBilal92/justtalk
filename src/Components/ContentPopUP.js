dc7932ba12699cd9d4d3a98894af2983488a3c26
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-28 13:33:56 +0500

    Create ContentPopUP.js

---
+++ src/Components/ContentPopUP.js
@@ -0,0 +1,94 @@
+//import liraries
+import { useState } from "react";
+import {
+	Button,
+	StyleSheet,
+	Modal,
+	View,
+	Text,
+	Image,
+	Dimensions,
+	SafeAreaView,
+} from "react-native";
+import navigationStrings from "../Constants/navigationStrings";
+import ImagesPath from "../Constants/ImagesPath";
+import BtnComp from "./btnComp";
+const { width } = Dimensions.get("window");
+
+// create a component
+const ContentPopUp = ({ navigation }) => {
+	const [isModalVisible, setModalVisible] = useState(true);
+
+	const toggleModalVisibility = () => {
+		setModalVisible(!isModalVisible);
+		// navigation.navigate("signup");
+	};
+
+	return (
+		<SafeAreaView>
+			<Modal
+				animationType="slide"
+				transparent
+				visible={isModalVisible}
+				presentationStyle="overFullScreen"
+			>
+				<View style={styles.viewWrapper}>
+					<View style={styles.modalView}>
+						<Image
+							style={styles.doneImage}
+							source={ImagesPath.confirmationDone}
+						/>
+						<Text style={styles.heading}>
+							Password reset link sent to your email
+						</Text>
+						<BtnComp
+							name="Continue"
+							onPress={{}}
+							buttonColor="#363333"
+							textColor="white"
+						></BtnComp>
+					</View>
+				</View>
+			</Modal>
+		</SafeAreaView>
+	);
+};
+
+// define your styles
+const styles = StyleSheet.create({
+	viewWrapper: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "rgba(0, 0, 0, 0.2)",
+	},
+	modalView: {
+		alignItems: "center",
+		// justifyContent: "center",
+		// position: "absolute",
+		top: "40%",
+		left: "40%",
+		elevation: 5,
+		transform: [{ translateX: -(width * 0.4) }, { translateY: -90 }],
+		height: 400,
+		width: width,
+		backgroundColor: "#fff",
+		borderRadius: 60,
+	},
+	doneImage: {
+		width: 100,
+		height: 100,
+		marginVertical: 30,
+	},
+	heading: {
+		fontSize: 24,
+		fontStyle: "Bold",
+		fontWeight: "bold",
+		marginVertical: 25,
+		textAlign: "center",
+		marginHorizontal: 70,
+	},
+});
+
+//make this component available to the app
+export default ContentPopUp;
