255f43ca70d4175c1885081729facc190bd75500
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-29 01:09:59 +0500

    Update newaccount.js

--- src/Screens/New Account/newaccount.js
+++ src/Screens/New Account/newaccount.js
@@ -1,61 +1,57 @@
 import React, { useState } from "react";
-import { View, Text, Image, TextInput, TouchableOpacity, } from "react-native";
-import ContentPopUp from "../../Components/ContentPopUP";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import styles from "../styles";
 import { useNavigation } from "@react-navigation/native";
 import ImagesPath from "../../Constants/ImagesPath";
 const NewAccount = () => {
-  const [handleshow, setHandleshow] = useState(false);
+	const [handleshow, setHandleshow] = useState(false);
 
-  const navigation = useNavigation();
-  return (
-    <View style={styles.myStyle}>
-      <Image source={ImagesPath.iconImage} style={{ marginTop: 100 }} />
-      <Text style={styles.txt}>Welcome to Signup!</Text>
-      <View>
-        <Text>Username</Text>
+	const navigation = useNavigation();
+	return (
+		<View style={styles.myStyle}>
+			<Image source={ImagesPath.iconImage} style={{ marginTop: 100 }} />
+			<Text style={styles.txt}>Welcome to Signup!</Text>
+			<View>
+				<Text>Username</Text>
 
-        <TextInput style={styles.TextInput} placeholder="Username" />
-      </View>
-      <View>
-        <Text>Password</Text>
-        <TextInput
-          style={styles.TextInput}
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-      </View>
-      <View>
-        <Text>Confirm Password</Text>
-        <TextInput
-          style={styles.TextInput}
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.ft1}> Accept T&C, Privacy Policy</Text>
+				<TextInput style={styles.TextInput} placeholder="Username" />
+			</View>
+			<View>
+				<Text>Password</Text>
+				<TextInput
+					style={styles.TextInput}
+					secureTextEntry={true}
+					placeholder="Password"
+				/>
+			</View>
+			<View>
+				<Text>Confirm Password</Text>
+				<TextInput
+					style={styles.TextInput}
+					secureTextEntry={true}
+					placeholder="Password"
+				/>
+			</View>
+			<View style={styles.row}>
+				<Text style={styles.ft1}> Accept T&C, Privacy Policy</Text>
+			</View>
 
-
-      </View>
-
-      <View>
-        <TouchableOpacity style={styles.button1} >
-          <Text style={styles.ft2}>SIGNUP</Text>
-        </TouchableOpacity>
-
-      </View>
-      <View style={styles.fv}>
-        <Text style={styles.acc}>
-          Already have an account?
-        </Text>
-        <TouchableOpacity style={styles.button3} onPress={() => navigation.navigate("login")} >
-          <Text style={styles.ft3}>LOGIN</Text>
-        </TouchableOpacity>
-
-      </View>
-    </View>
-  );
+			<View>
+				<TouchableOpacity style={styles.button1}>
+					<Text style={styles.ft2}>SIGNUP</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.fv}>
+				<Text style={styles.acc}>Already have an account?</Text>
+				<TouchableOpacity
+					style={styles.button3}
+					onPress={() => navigation.navigate("login")}
+				>
+					<Text style={styles.ft3}>LOGIN</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 };
 
 //make this component available to the app
