7058b01c2c555ce3d24e9ac96d9928af5989e92c
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-29 03:14:38 +0500

    Update Login.js

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,9 +1,17 @@
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+	Button,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
 import React from "react";
 import styles from "../styles";
 import ImagesPath from "../../Constants/ImagesPath";
 import { Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import navigationStrings from "../../Constants/navigationStrings";
 
 const Login = () => {
 	const navigation = useNavigation();
@@ -27,25 +35,36 @@ const Login = () => {
 			<View style={styles.row}>
 				<Text style={styles.ft1}> Remember me</Text>
 
-				<TouchableOpacity style={styles.button} onPress={() => navigation.navigate("ForgotPassword")}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => {
+						navigation.navigate(navigationStrings.FORGOTPASSWORD);
+					}}
+				>
 					<Text style={styles.ft}>Forgot Password?</Text>
 				</TouchableOpacity>
 			</View>
 
 			<View>
-				<TouchableOpacity style={styles.button1} >
+				<TouchableOpacity
+					style={styles.button1}
+					onPress={() => {
+						navigation.navigate(navigationStrings.HOME);
+					}}
+				>
 					<Text style={styles.ft2}>LOG IN</Text>
 				</TouchableOpacity>
-
 			</View>
 			<View style={styles.fv}>
-				<Text style={styles.acc}>
-					Don't have an account?
-				</Text>
-				<TouchableOpacity style={styles.button3} onPress={() => navigation.navigate("newaccount")} >
+				<Text style={styles.acc}>Don't have an account?</Text>
+				<TouchableOpacity
+					style={styles.button3}
+					onPress={() => {
+						navigation.navigate(navigationStrings.NEWACCOUNT);
+					}}
+				>
 					<Text style={styles.ft3}>CREATE AN ACCOUNT</Text>
 				</TouchableOpacity>
-
 			</View>
 		</View>
 	);
