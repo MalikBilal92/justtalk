3b626562a5e38f5caebcb68fb840f0009943a5ed
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-28 20:55:35 +0500

    Update Home.js

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
-import StatusPopUp from "../../Components/StatusPopUP";
+import ContentPopUp from "../../Components/ContentPopUp";
 import styles from "../styles";
 const Home = () => {
-	const [myNum, setNum] = useState(10);
-	const changeNum = () => {
-		setNum(20);
-	};
 	const [handleshow, setHandleshow] = useState(false);
 	console.log("numValue", myNum);
 
@@ -14,7 +10,7 @@ const Home = () => {
 		<View style={styles.myStyle}>
 			<Text>Please subscribe my channel</Text>
 			<Button title="Press me" onPress={() => setHandleshow(!handleshow)} />
-			{handleshow && <StatusPopUp />}
+			{handleshow && <ContentPopUp />}
 		</View>
 	);
 };
