fae46cf2840fb46644983f828c0e1281d298faac
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-29 15:14:16 +0500

    Update ForgotPassword.js

--- src/Screens/ForgotPassword/ForgotPassword.js
+++ src/Screens/ForgotPassword/ForgotPassword.js
@@ -1,26 +1,34 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import styles from "../styles";
-
+import StatusPopUp from "../../Components/StatusPopUp";
 const ForgotPassword = () => {
-    return (
-        <View>
-            <Text style={styles.text}>Enter your email to reset your password</Text>
-            <Text style={styles.txtt} >Email</Text>
-            <View style={styles.views}>
+	const [handleshow, setHandleshow] = useState(false);
+	return (
+		<View>
+			<Text style={styles.text}>Enter your email to reset your password</Text>
+			<Text style={styles.txtt}>Email</Text>
+			<View style={styles.views}>
+				<TextInput style={styles.TextInput} placeholder="Email" />
+			</View>
+			<TouchableOpacity
+				style={styles.button4}
+				onPress={() => setHandleshow(!handleshow)}
+			>
+				<Text style={styles.ft2}>SEND PASSWORD RESET LINK</Text>
+			</TouchableOpacity>
+			{handleshow && <StatusPopUp />}
+		</View>
+	);
+};
 
-
-                <TextInput style={styles.TextInput} placeholder="Email" />
-            </View>
-            <TouchableOpacity style={styles.button4} >
-                <Text style={styles.ft2}>SEND PASSWORD RESET LINK</Text>
-            </TouchableOpacity>
-
-        </View>
-    )
-}
-
-export default ForgotPassword
+export default ForgotPassword;
 
 // const styles = StyleSheet.create({
 
