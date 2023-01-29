04e9afa78f093bc9fe6bd6a4773d28dac5676ad9
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-30 01:07:53 +0500

    Update ContentPopUP.js

--- src/Components/ContentPopUP.js
+++ src/Components/ContentPopUP.js
@@ -20,7 +20,7 @@ const ContentPopUp = ({ name, Description, image, data }) => {
 	const navigation = useNavigation();
 	const toggleModalVisibility = () => {
 		setModalVisible(!isModalVisible);
-		navigation.navigate(navigationStrings.HOME);
+		navigation.navigate(navigationStrings.HOME, { data: data });
 	};
 
 	return (
