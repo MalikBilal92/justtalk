8054bd0aff5635a9eb719f38dbc1532b9a8bb0fc
MalikBilal92 <123270296+MalikBilal92@users.noreply.github.com>
2023-01-29 15:14:34 +0500

    Update SignUp.js

--- src/Screens/SignUp/SignUp.js
+++ src/Screens/SignUp/SignUp.js
@@ -1,12 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import styles from "../styles";
-const Signup = () => {
+import { useNavigation } from "@react-navigation/native";
+import ImagesPath from "../../Constants/ImagesPath";
+import navigationStrings from "../../Constants/navigationStrings";
+const SignUp = () => {
+	const [handleshow, setHandleshow] = useState(false);
+
+	const navigation = useNavigation();
 	return (
 		<View style={styles.myStyle}>
-			<Text>signup</Text>
+			<Image source={ImagesPath.iconImage} style={{ marginTop: 100 }} />
+			<Text style={styles.txt}>Welcome to Signup!</Text>
+			<View>
+				<Text>Username</Text>
+
+				<TextInput style={styles.TextInput} placeholder="Username" />
+			</View>
+			<View>
+				<Text>Password</Text>
+				<TextInput
+					style={styles.TextInput}
+					secureTextEntry={true}
+					placeholder="Password"
+				/>
+			</View>
+			<View>
+				<Text>Confirm Password</Text>
+				<TextInput
+					style={styles.TextInput}
+					secureTextEntry={true}
+					placeholder="Password"
+				/>
+			</View>
+			<View style={styles.row}>
+				<Text style={styles.ft1}> Accept T&C, Privacy Policy</Text>
+			</View>
+
+			<View>
+				<TouchableOpacity
+					style={styles.button1}
+					onPress={() => navigation.navigate(navigationStrings.LOGIN)}
+				>
+					<Text style={styles.ft2}>SIGNUP</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.fv}>
+				<Text style={styles.acc}>Already have an account?</Text>
+				<TouchableOpacity
+					style={styles.button3}
+					onPress={() => navigation.navigate(navigationStrings.LOGIN)}
+				>
+					<Text style={styles.ft3}>LOGIN</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
 
-export default Signup;
+//make this component available to the app
+export default SignUp;
